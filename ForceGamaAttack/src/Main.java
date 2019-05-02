a9b2f8e497f9de10100901e30f569d76bc49ee82
Isaac <isaacbmota@gmail.com>
2019-05-02 15:54:54 -0300

    Creating a 800x600 window

---
+++ ForceGamaAttack/src/Main.java
@@ -0,0 +1,11 @@
+import jplay.Window;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("It's running!");
+		
+		Window window = new Window(800,600);
+	}
+}
