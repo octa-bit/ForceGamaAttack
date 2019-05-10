8f45879f63127c0563f77c995cda2d072170aaff
Eduardo Lima <eduardolimrib@gmail.com>
2019-05-10 09:36:03 -0300

    Update main

--- ForceGamaAttack/src/Main.java
+++ ForceGamaAttack/src/Main.java
@@ -1,21 +1,12 @@
-import jplay.Window;
-import jplay.GameImage;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		System.out.println("It's running!");
-		
-		Window window = new Window(800,600);
-		
-		GameImage background = new GameImage("src/graphics/img/temporary.png");
-		
-		//basic game loop		
-		while(true) {
-			
-			background.draw();
-			window.update();
-		}
+
+		Game game = new Game();
+		game.runGame();
 	}
 }
