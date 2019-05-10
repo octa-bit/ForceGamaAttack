6df42ce4c45d0cb86640e0f9daaf7bf121729fee
Eduardo Lima <eduardolimrib@gmail.com>
2019-05-10 09:37:37 -0300

    New game class
    Co-authored-by: Lucas362 <lucasvitorifg@gmail.com>
    Co-authored-by: Rossicler <rossiclerjr@gmail.com>

---
+++ ForceGamaAttack/src/Game.java
@@ -0,0 +1,26 @@
+import jplay.Window;
+import jplay.GameImage;
+
+public class Game {
+	GameState state;
+	Window window;
+	GameImage background;
+	
+	public enum GameState{
+		Running, Paused, Stopped;
+	}
+	
+	public Game() {
+		this.window = new Window(800,600);
+		this.background = new GameImage("src/graphics/img/temporary.png");
+		this.state = GameState.Running;
+	}
+	
+	public void runGame() {
+		while(this.state == GameState.Running) {
+			this.background.draw();
+			this.window.update();
+		}
+	}
+
+}
