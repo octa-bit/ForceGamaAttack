8a44b1af5a1f75c395053a6b47366338ffefd296
João Lucas Sousa Reis <joao.lucas.ssr@gmail.com>
2019-05-20 12:19:17 -0300

    Starting Spaceship movement
    Co-authored-by: joaovitorml <joao.lemos4000@gmail.com>

--- ForceGamaAttack/src/player/Player.java
+++ ForceGamaAttack/src/player/Player.java
@@ -1,5 +1,17 @@
 package player;
 
 public class Player {
+	private PlayerSpaceship spaceship;
 
+	public PlayerSpaceship getSpaceship() {
+		
+		if (spaceship == null) {
+			spaceship = new PlayerSpaceship(this, this.initialPositionY, this.initialPositionY, true);
+		}
+		return spaceship;
+	}
+
+	public void setSpaceship(PlayerSpaceship spaceship) {
+		this.spaceship = spaceship;
+	}
 }
---
+++ ForceGamaAttack/src/player/PlayerSpaceship.java
@@ -0,0 +1,17 @@
+package player;
+
+import jplay.Keyboard;
+
+public class PlayerSpaceship {
+	private static final String spriteImagePath = "src/graphics/img/SpaceshipWhiteNoT.gif";
+	
+	private Player player;
+	
+	private int upKey = Keyboard.UP_KEY;
+	private int downKey = Keyboard.DOWN_KEY;
+	private int leftKey = Keyboard.LEFT_KEY;
+	private int rightKey = Keyboard.RIGHT_KEY;
+
+	
+	public 
+}
