475c228dd0387d8d54b5ad4c7877a3bd0c4c4551
João VItor Morandi Lemos <joao.lemos4000@gmail.com>
2019-05-21 15:25:46 -0300

    Adding spaceship moving keys.
    Co-authored-by: jlucassr <joao.lucas.ssr@gmail.com>

--- ForceGamaAttack/src/scenes/game/GameScene.java
+++ ForceGamaAttack/src/scenes/game/GameScene.java
@@ -1,30 +1,30 @@
 package scenes.game;
 import scenes.Scene;
 import jplay.GameImage;
-import jplay.GameObject;
+import jplay.Sprite;
 import jplay.Keyboard;
 import player.Player;
 
 public class GameScene extends Scene {
 	private GameImage background;
 	private GameImage player;
-	private GameObject player1;
 	
 	protected void initialSetup(){
 		keyboard.setBehavior(Keyboard.DOWN_KEY, Keyboard.DETECT_INITIAL_PRESS_ONLY);
 		keyboard.setBehavior(Keyboard.UP_KEY, Keyboard.DETECT_INITIAL_PRESS_ONLY);
+		keyboard.setBehavior(Keyboard.LEFT_KEY, Keyboard.DETECT_INITIAL_PRESS_ONLY);
+		keyboard.setBehavior(Keyboard.RIGHT_KEY, Keyboard.DETECT_INITIAL_PRESS_ONLY);
 	}
 	
 	protected void viewSetup(){
 		background = new GameImage("src/graphics/img/space_bg.jpg");
-		player = new GameImage("src/graphics/img/SpaceshipWhiteNoT.gif");
+		player = new Sprite("src/graphics/img/SpaceshipWhiteNoT.gif",10);
 		player.x = 360.0;
 		player.y = 550.0;
 		player.height = 40;
 		player.width = 40;
 	}
 	
-
 	private void draw() {
 		background.draw();
 		player.draw();
@@ -32,6 +32,7 @@ public class GameScene extends Scene {
 
 	public void update(){
 		draw();
+		((Sprite) player).moveY(10.0);
+		((Sprite) player).moveX(10.0);
 	}
-	
 }
