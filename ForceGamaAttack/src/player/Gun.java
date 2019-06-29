903c7b5561d930d5eb98fa1d7d5f199a808cd2dd
lucas <lucasvitorifg@gmail.com>
2019-06-29 17:03:57 -0300

    Adding shoot time to gun
    Co-authored-by: Rossicler <rossiclerjr@gmail.com>
    Co-authored-by: Viana <gabrielv14@gmail.com>

--- ForceGamaAttack/src/player/Gun.java
+++ ForceGamaAttack/src/player/Gun.java
@@ -1,12 +1,37 @@
 package player;
 
+import java.time.Duration;
+import java.time.Instant;
+
 public class Gun {
 	int type;
 	int damage;
 	int shootDamage;
 	int shootingSpeed;
 	int powerUp;
+	private Instant lastShootTime;
 	
+	public Gun(int shootingSpeed) {
+		this.shootingSpeed = shootingSpeed;
+		lastShootTime = Instant.now();
+	}
 	public void activePowerUp() {
+	}
+
+	public Instant getLastShootTime() {
+		return lastShootTime;
+	}
+	
+	public boolean canShoot() {
+		Instant actualShoot = Instant.now();
+		boolean canShoot = (Duration.between(lastShootTime, actualShoot).toMillis()) > shootingSpeed;
+		if(canShoot) {
+			lastShootTime = actualShoot;
+		}
+		return canShoot;
+	}
+
+	public void setLastShootTime(Instant time) {
+		lastShootTime = time;
 	}
 }
