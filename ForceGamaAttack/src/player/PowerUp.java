aa72de0e4456cd3115e10719cb91273eb55e5cf7
Eduardo Lima <eduardolimrib@gmail.com>
2019-06-30 21:00:46 -0300

    Updated powerups
    Co-authored-by: Lucas362 <lucasvitorifg@gmail.com>

--- ForceGamaAttack/src/player/PowerUp.java
+++ ForceGamaAttack/src/player/PowerUp.java
@@ -1,12 +1,48 @@
 package player;
 
-public class PowerUp {
-	int type;
-	int name;
-	int duration;
+import jplay.Sprite;
+
+public class PowerUp extends Sprite{
+	public enum PowerUpType {
+		HAPPINESS , TEP, IHCS2, MAUMI, ROCHA;
+	}
+	public enum PowerUpState {
+		ACTIVATED, DEACTIVATED;
+	}
+	
+	private static String sprite = "src/graphics/img/top.png";
+	
+	private PowerUpState state;
+	private PowerUpType type;
+	private String name;
+	private int duration;
+	
+	public PowerUp(String sprite, float x, float y,
+				   PowerUpType type, String name, int duration) {
+		super(sprite);
+		this.x = x;
+		this.y = y;
+		this.type = type;
+		this.name = name;
+		this.duration = duration;
+		this.state = PowerUpState.ACTIVATED;
+	}
+	
+	public void setPosition(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void move() {
+		this.y += 0.6;
+	}
 	
 	public boolean isActivated() {
-		return false;
-		
+		if(this.state == PowerUpState.ACTIVATED) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 }
