60efc62a330326cb19ec5fe39eb4da5e38dbe21f
Gabriel Viana <gabrielv14@gmail.com>
2019-07-01 01:05:57 -0300

    Structure Refactoring

--- ForceGamaAttack/src/player/StructureStrategyJava.java
+++ ForceGamaAttack/src/player/StructureStrategyJava.java
@@ -1,5 +1,9 @@
 package player;
 
+import java.awt.event.KeyEvent;
+
+import constants.WindowConstants;
+
 public class StructureStrategyJava extends Structure {
 	
 	private Player player;
@@ -11,5 +15,31 @@ public class StructureStrategyJava extends Structure {
 		this.height = 40;
 		this.width = 46;
 		this.speed = 0.5;
+		this.bulletSprite = "src/graphics/img/java-beam.png";
+		this.gunShootingSpeed = 333;
+		this.gunDamage = 50;
+	}
+	
+	@Override
+	public void moveX(double x) {
+		if(this.getKeyboard().keyDown(KeyEvent.VK_RIGHT) &&
+		   this.x + this.width < WindowConstants.WIDTH) {
+			this.x += x / 2;
+		}
+		else if(this.getKeyboard().keyDown(KeyEvent.VK_LEFT) &&
+				this.x > 0) {
+			this.x -= x / 2;
+		}
+	}
+	@Override
+	public void moveY(double y) {
+		if(this.getKeyboard().keyDown(KeyEvent.VK_DOWN) &&
+		   this.y + this.height < WindowConstants.HEIGHT) {
+			this.y += y / 2;
+		}
+		else if(this.getKeyboard().keyDown(KeyEvent.VK_UP) &&
+				this.y > 0) {
+			this.y -= y / 2;
+		}
 	}
 }
